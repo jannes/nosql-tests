b8ce77a016c2d57342c4f8d272d772ffaeaad01e
jannes <jannes.dev@posteo.net>
2020-02-20 12:23:37 +0100

    start and stop 2 extra replicas

--- startdb.sh
+++ startdb.sh
@@ -14,7 +14,9 @@ echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
 
 ulimit -n 60000
 
-WATCHER_PID=/tmp/watcher.pid
+WATCHER_PID0=/tmp/watcher0.pid
+WATCHER_PID1=/tmp/watcher1.pid
+WATCHER_PID2=/tmp/watcher2.pid
 
 # comm cputime etimes rss pcpu
 export AWKCMD='{a[$1] = $1; b[$1] = $2; c[$1] = $3; d[$1] = $4; e[$1] = $5} END {for (i in a) printf "%s; %s; %s; %0.1f; %0.1f\n", a[i], b[i], c[i], d[i], e[i]}'
@@ -38,16 +40,19 @@ killPIDFile() {
 }
 
 stop_MongoDB() {
-    killPIDFile "/var/tmp/mongodb.pid"
+    killPIDFile "/var/tmp/mongodb0.pid"
+    killPIDFile "/var/tmp/mongodb1.pid"
+    killPIDFile "/var/tmp/mongodb2.pid"
 }
 
-start_MongoDB() {
+start_MongoDB_Master() {
     numactl --interleave=all \
-        ${DBFOLDER}/mongodb/bin/mongod \
+        ${DBFOLDER}/mongodb/bin/mongod42mod \
         --bind_ip 0.0.0.0 \
+        --port 27017 \
         --fork \
-        --logpath /var/tmp/mongodb.log \
-        --pidfilepath /var/tmp/mongodb.pid \
+        --logpath /var/tmp/mongodb0.log \
+        --pidfilepath /var/tmp/mongodb0.pid \
         --storageEngine wiredTiger \
         --dbpath ${DBFOLDER}/mongodb/pokec
 
@@ -58,20 +63,74 @@ while true; do
     ps -C mongod -o 'comm cputime etimes rss pcpu' --no-headers | \
         awk '${AWKCMD}'
 done  > $FN 2>&1 " > /dev/null 2>&1 &
-    echo "$!" > "${WATCHER_PID}"
+    echo "$!" > "${WATCHER_PID0}"
 }
 
+start_MongoDB_Replica1() {
+    numactl --interleave=all \
+        ${DBFOLDER}/mongodb/bin/mongod42mod \
+        --bind_ip 0.0.0.0 \
+        --port 27018 \
+        --fork \
+        --logpath /var/tmp/mongodb1.log \
+        --pidfilepath /var/tmp/mongodb1.pid \
+        --storageEngine wiredTiger \
+        --dbpath ${DBFOLDER}/mongodb/pokec1
+
+    nohup bash -c "
+while true; do
+    sleep 1
+    echo -n \"`date`; \"
+    ps -C mongod -o 'comm cputime etimes rss pcpu' --no-headers | \
+        awk '${AWKCMD}'
+done  > $FN 2>&1 " > /dev/null 2>&1 &
+    echo "$!" > "${WATCHER_PID1}"
+}
+
+start_MongoDB_Replica2() {
+    numactl --interleave=all \
+        ${DBFOLDER}/mongodb/bin/mongod42mod \
+        --bind_ip 0.0.0.0 \
+        --port 27019 \
+        --fork \
+        --logpath /var/tmp/mongodb2.log \
+        --pidfilepath /var/tmp/mongodb2.pid \
+        --storageEngine wiredTiger \
+        --dbpath ${DBFOLDER}/mongodb/pokec2
+
+    nohup bash -c "
+while true; do
+    sleep 1
+    echo -n \"`date`; \"
+    ps -C mongod -o 'comm cputime etimes rss pcpu' --no-headers | \
+        awk '${AWKCMD}'
+done  > $FN 2>&1 " > /dev/null 2>&1 &
+    echo "$!" > "${WATCHER_PID1}"
+}
+
+start_MongoDB_Replicas() {
+    start_MongoDB_Replica1
+    start_MongoDB_Replica2
+}
 
 echo "================================================================================"
-echo "* stopping mongod"
+echo "* stopping mongod instances"
 echo "================================================================================"
 
 stop_MongoDB
 
-killPIDFile "${WATCHER_PID}"
+killPIDFile "${WATCHER_PID0}"
+killPIDFile "${WATCHER_PID1}"
+killPIDFile "${WATCHER_PID2}"
 
 echo "================================================================================"
 echo "* starting: $which $version"
 echo "================================================================================"
 
-start_MongoDB
+start_MongoDB_Master
+
+echo "================================================================================"
+echo "* starting replicas"
+echo "================================================================================"
+
+start_MongoDB_Replicas
--- stopdb.sh
+++ stopdb.sh
@@ -10,7 +10,9 @@ echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle
 echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
 "
 
-WATCHER_PID=/tmp/watcher.pid
+WATCHER_PID0=/tmp/watcher0.pid
+WATCHER_PID1=/tmp/watcher1.pid
+WATCHER_PID2=/tmp/watcher2.pid
 export AWKCMD='{a[$1] = $1; b[$1] = $2; c[$1] = $3; d[$1] = $4}END{for (i in a)printf "%s, %s, %s, %0.1f\n", a[i], b[i], c[i], d[i]}'
 
 killPIDFile() {
@@ -32,13 +34,17 @@ killPIDFile() {
 }
 
 stop_MongoDB() {
-    killPIDFile "/var/tmp/mongodb.pid"
+    killPIDFile "/var/tmp/mongodb0.pid"
+    killPIDFile "/var/tmp/mongodb1.pid"
+    killPIDFile "/var/tmp/mongodb2.pid"
 }
 
 
 echo "================================================================================"
-echo "* stopping mongod"
+echo "* stopping mongod instances"
 echo "================================================================================"
 
 stop_MongoDB
-killPIDFile "${WATCHER_PID}"
+killPIDFile "${WATCHER_PID0}"
+killPIDFile "${WATCHER_PID1}"
+killPIDFile "${WATCHER_PID2}"
